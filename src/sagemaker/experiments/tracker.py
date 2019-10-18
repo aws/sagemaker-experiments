b088a5f4397f77242ec8be623bd4ebdfc41063cd
Glenn Danthi <gddanthi@amazon.com>
2019-10-18 01:29:32 -0700

    Basic tracker creation logic

--- src/sagemaker/experiments/tracker.py
+++ src/sagemaker/experiments/tracker.py
@@ -13,11 +13,85 @@
 """Placeholder docstring"""
 from __future__ import absolute_import
 
+import os
+import sys
+import uuid
+import boto3
+
 
 class Tracker(object):
     """
     Create a tracker to log metrics and artifacts.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, name=None, display_name=None, source_arn=None, sagemaker_boto_client=None):
+        self.sagemaker_boto_client = sagemaker_boto_client or self._default_sagemaker_boto_client()
+        self.source_arn = source_arn or self._resolve_source_arn()
+        self.failed_mode = False
+        self.component_name = None
+
+        if self.source_arn and self._has_auto_created_trial_component(self.source_arn):
+            try:
+                self.component_name = self._get_component_name(self.source_arn)
+            except:
+                # We do not want training job to fail if we cant find the auto-created component
+                e = sys.exc_info()
+                print("Unable to get the trial component name for "
+                      "source arn : {} due to : {}".format(self.source_arn, str(e)))
+                self.failed_mode = True
+        else:
+            if not display_name:
+                raise ValueError("Please specify a valid display_name.")
+            self.component_name = name if name else self._generate_component_name(display_name)
+            self._create_trial_component(self.component_name, display_name, self.source_arn)
+
+    def _create_trial_component(self, component_name, display_name, source_arn):
+        """Placeholder docstring"""
+        self.sagemaker_boto_client.create_trial_component(
+            TrialComponentName=component_name,
+            DisplayName=display_name,
+            Source={"SourceArn": source_arn},
+        )
+
+    @staticmethod
+    def _generate_component_name(display_name):
+        """Placeholder docstring"""
+        return "{}-{}".format(display_name, str(uuid.uuid4()))
+
+    @staticmethod
+    def _resolve_source_arn():
+        """
+        Resolves the source arn from the environment
+        """
+        source_arn_env_keys = ["TRAINING_JOB_ARN"]
+        return next(
+            (
+                os.environ.get(source_arn_env_key)
+                for source_arn_env_key in source_arn_env_keys
+                if os.environ.get(source_arn_env_key)
+            ),
+            None,
+        )
+
+    def _get_component_name(self, source_arn):
+        response = self.sagemaker_boto_client.list_trial_components(
+            SourceArn=source_arn
+        )
+        component_summaries = response.get("TrialComponents")
+        # There should only be one trial component for a given source arn
+        summary = component_summaries[0]
+        return summary.get("TrialComponentName")
+
+    @staticmethod
+    def _default_sagemaker_boto_client():
+        """Placeholder docstring"""
+        return boto3.client("sagemaker")
+
+    @staticmethod
+    def _has_auto_created_trial_component(source_arn):
+        """Placeholder docstring"""
+        auto_created_types = ["training-job", "tuning-job", "transform-job", "analytics-job"]
+        for entity_type in auto_created_types:
+            if "{}/".format(entity_type) in source_arn:
+                return True
+        return False
--- tests/unit/test_tracker.py
+++ tests/unit/test_tracker.py
@@ -12,10 +12,88 @@
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
+import os
+import pytest
+import mock
+
 from sagemaker.experiments.tracker import Tracker
 
 
-# FIXME: Dummy test. Remove when tracker class is populated
-def test_tracker():
-    tracker = Tracker()
-    assert tracker
+@pytest.fixture
+def mock_boto_client():
+    return mock.Mock()
+
+
+@mock.patch("boto3.client")
+def test_tracker_gets_default_boto_client_if_none_supplied(boto_client):
+    tracker = Tracker(display_name="PreProcessing")
+    boto_client.assert_called_once_with("sagemaker")
+    assert tracker.sagemaker_boto_client
+
+
+def test_create_tracker_in_training_job(mock_boto_client):
+    training_job_arn = "arn:aws:sagemaker:test:1234:training-job/abcd"
+
+    mock_boto_client.list_trial_components.return_value = {
+        "TrialComponents": [
+            {
+                "TrialComponentName": "abcd",
+                "TrialComponentArn": "arn:aws:sagemaker:test:1234:experiment-trial-component/abcd"
+            }
+        ]
+    }
+
+    with mock.patch.dict(os.environ, {"TRAINING_JOB_ARN": training_job_arn}):
+        tracker = Tracker(sagemaker_boto_client=mock_boto_client)
+
+    assert tracker.source_arn == training_job_arn
+    assert tracker.component_name == "abcd"
+    assert not tracker.failed_mode
+    mock_boto_client.create_trial_component.assert_not_called()
+
+
+def test_create_tracker_in_training_job_failed_mode(mock_boto_client):
+    training_job_arn = "arn:aws:sagemaker:test:1234:training-job/abcd"
+
+    mock_boto_client.list_trial_components.side_effect = RuntimeError()
+
+    with mock.patch.dict(os.environ, {"TRAINING_JOB_ARN": training_job_arn}):
+        tracker = Tracker(sagemaker_boto_client=mock_boto_client)
+
+    assert tracker.source_arn == training_job_arn
+    assert tracker.failed_mode
+    mock_boto_client.create_trial_component.assert_not_called()
+
+
+def test_create_tracker_in_notebook(mock_boto_client):
+    notebook_arn = "arn:aws:sagemaker:test:1234:notebook-instance/abcd"
+    tracker = Tracker(
+        display_name="Training", source_arn=notebook_arn, sagemaker_boto_client=mock_boto_client
+    )
+
+    assert tracker.source_arn == notebook_arn
+    assert tracker.component_name.startswith("Training")
+    assert not tracker.failed_mode
+    mock_boto_client.create_trial_component.assert_called_once_with(
+        TrialComponentName=tracker.component_name,
+        DisplayName="Training",
+        Source={"SourceArn": notebook_arn},
+    )
+
+
+def test_create_tracker_in_notebook_no_source_arn(mock_boto_client):
+    tracker = Tracker(display_name="PreProcessing", sagemaker_boto_client=mock_boto_client)
+
+    assert not tracker.source_arn
+    assert tracker.component_name.startswith("PreProcessing")
+    assert not tracker.failed_mode
+    mock_boto_client.create_trial_component.assert_called_once_with(
+        TrialComponentName=tracker.component_name,
+        DisplayName="PreProcessing",
+        Source={"SourceArn": None},
+    )
+
+
+def test_create_tracker_in_notebook_no_display_name(mock_boto_client):
+    with pytest.raises(ValueError):
+        Tracker(sagemaker_boto_client=mock_boto_client)
